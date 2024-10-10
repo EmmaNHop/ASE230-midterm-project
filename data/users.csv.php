c85ab1aa7fee233baa833c6379384a7aa05a2e88
LeahJuddNKU <112567213+LeahJuddNKU@users.noreply.github.com>
2024-10-10 13:54:09 -0400

    Update users.csv.php

--- data/users.csv.php
+++ data/users.csv.php
@@ -1,6 +1,1 @@
 <?php die() ?>
-iamtesting@testing.com;test;test;test
-iamtesting@testing.com;test;test;test
-iamtesting@testing.com;test;test;test
-asjeiof@tjaesio.com;test;test;test
-test@test.com;test;test;test
