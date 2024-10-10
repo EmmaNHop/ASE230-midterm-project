5269a6696766fc3e683e64bcc7ead6bb24e5da25
LeahJuddNKU <112567213+LeahJuddNKU@users.noreply.github.com>
2024-10-10 13:43:33 -0400

    Register Page Logic

---
+++ data/users.csv.php
@@ -0,0 +1,6 @@
+<?php die() ?>
+iamtesting@testing.com;test;test;test
+iamtesting@testing.com;test;test;test
+iamtesting@testing.com;test;test;test
+asjeiof@tjaesio.com;test;test;test
+test@test.com;test;test;test
